b43c558d4633a20af640e7e4d7dec59041f77419
Qingyun Wang <lucas.qingyun@gmail.com>
2025-02-12 01:39:03 +0200

    ban setId()

--- GradeBook/src/main/java/model/Student.java
+++ GradeBook/src/main/java/model/Student.java
@@ -22,9 +22,9 @@ public class Student {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+//    public void setId(int id) {
+//        this.id = id;
+//    }
 
     public String getName() {
         return name;
