d6d07c605702dc0155e7a5886d3b1b18f9c3e483
yangyang <yang.yang@metropolia.fi>
2025-02-12 09:03:18 +0200

    test

---
+++ GradeBook/src/test/java/controller/GroupManageControllerTest.java
@@ -0,0 +1,29 @@
+package controller;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GroupManageControllerTest {
+
+    @Test
+    @Disabled
+    void initialize() {
+    }
+
+    @Test
+    @Disabled
+    void addNewGroup() {
+    }
+
+    @Test
+    @Disabled
+    void deleteGroupInfo() {
+    }
+
+    @Test
+    @Disabled
+    void modifyGroupInfo() {
+    }
+}
---
+++ GradeBook/src/test/java/controller/GroupModifyControllerTest.java
@@ -0,0 +1,24 @@
+package controller;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GroupModifyControllerTest {
+
+    @Test
+    @Disabled
+    void setGroupManageController() {
+    }
+
+    @Test
+    @Disabled
+    void loadGroupDetails() {
+    }
+
+    @Test
+    @Disabled
+    void refresh() {
+    }
+}
